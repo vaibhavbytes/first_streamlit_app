b804c8584a73d8a0b9e399bfa9d0371dcf7f0753
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-05 13:20:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
    
