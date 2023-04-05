d738daac038f34bf56da71b8495e87c2302ac096
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-05 13:15:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+
+streamlit.title("My parents new healthy diner")
+streamlit.header("Breakfast menu")
+streamlit.text("Omega3 and Blueberry oat meal")
+streamlit.text("Kale, spinach and Rocket smoothie")
+streamlit.text("Hard boiled free range eggs")
 
