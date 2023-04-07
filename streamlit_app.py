e0e3168973807cebcec1ebbd6495a38c306bf550
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-07 10:21:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -25,7 +24,7 @@ import requests
 # Display the table on the page.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-
+streamlit.text(fruityvice_response)
 
 
 
