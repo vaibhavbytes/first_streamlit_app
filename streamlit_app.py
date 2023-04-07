e3615939f5d9b6dfc017667da3a14747fbce2434
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-07 23:07:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,13 @@ import pandas
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor() 
+
+con = snowflake.connector.connect(
+    user='vaibhavbytes',
+    password='Sandhy@99',
+    account='WL02737.us-east-2.aws'
+)
+my_cur = con.cursor() 
 my_cur.execute("use warehouse pc_rivery_wh; use role pc_rivery_role")
 my_cur.execute("Select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
