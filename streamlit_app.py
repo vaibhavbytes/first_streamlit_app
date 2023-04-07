b7e142bfcb4999dd3b58a59192633143ea336302
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-07 23:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("Select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
