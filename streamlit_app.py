27f6159bb9029849c6f206874ee784f3b8e3068d
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-08 11:37:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.header("My Mom's new healthier Diner")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title("My parents new healthy diner")
 streamlit.header('🥣 Breakfast favourites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
