1f08361eeadc66923e95b057569ab1e442c05b8f
vaibhavbytes <129886671+vaibhavbytes@users.noreply.github.com>
2023-04-08 20:00:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 streamlit.header('🥣 Fruityvise fruit advice !!!')
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like to add ?','Kiwi')
+   fruit_choice = streamlit.text_input('What fruit would you like to add ?')
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
    else:
@@ -47,3 +47,18 @@ try:
       streamlit.dataframe(fruit_choice)
 except:
   streamlit.write("An exception occurred")
+
+def insert_row_snowflake(new_fruit):
+   with my_cnx_cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return "Thanks for adding "+new_fruit
+   
+   add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
+   if streamlit.button('add a fruit to the list')
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      back_from_function = insert_row_snowflake(add_my_fruit)
+      streamlit.text(back_from_function)
+      
+      
+      
+   
